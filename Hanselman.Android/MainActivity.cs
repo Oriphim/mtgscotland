8f9acae1b09a2c16125add132d0b262d1b140ba4
Henry Tsang <Htsang@tactuum.com>
2016-10-03 09:48:42 +0100

    first commit

---
+++ Hanselman.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+using Android.Content.PM;
+using Hanselman.Portable;
+using Android.Graphics.Drawables;
+using ImageCircle.Forms.Plugin.Droid;
+
+namespace HanselmanAndroid
+{
+    [Activity(Label = "MTG Scotland",
+        MainLauncher = true,
+        ScreenOrientation = ScreenOrientation.Portrait,
+        ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : FormsAppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            FormsAppCompatActivity.ToolbarResource = Resource.Layout.toolbar;
+            FormsAppCompatActivity.TabLayoutResource = Resource.Layout.tabs;
+            base.OnCreate(bundle);
+            Forms.Init(this, bundle);
+            ImageCircleRenderer.Init();
+            LoadApplication(new App());
+
+         
+        }
+    }
+}
---
+++ Hanselman.Portable/Models/HomeMenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Hanselman.Portable
+{
+    public enum MenuType
+    {
+        About,
+        Blog,
+        Twitter,
+        Ratchet,
+        DeveloperLife
+    }
+    public class HomeMenuItem : BaseModel
+    {
+        public HomeMenuItem()
+        {
+            MenuType = MenuType.About;
+        }
+        public string Icon { get; set; }
+        public MenuType MenuType { get; set; }
+    }
+}
+
